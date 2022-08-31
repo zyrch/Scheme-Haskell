f9467f8ac861f84319c588ddd939a3f6f8650d85
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2022-09-01 02:44:08 +0530

    Added: Octal, Hexdecimal, Binary parsing, Bug Fix: escapedSequenceParsing

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 import System.Environment
+import Data.Char(digitToInt, ord, intToDigit, toUpper)
 
 data LispVal = Atom String
              | List [LispVal]
@@ -10,6 +11,7 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+             deriving Show
 
 main :: IO ()
 main = do
@@ -24,12 +26,20 @@ spaces :: Parser ()
 spaces = skipMany1 space
 
 parseEscapeSequence :: Parser Char
-parseEscapeSequence = char '\\' >> oneOf "nr\\t\""     -- oneOf nrt\"
+parseEscapeSequence = do
+  char '\\'
+  c <- oneOf "nr\\t\""     -- oneOf nrt\"
+  return $ case c of
+        '\\' -> c
+        '"'  -> c
+        'n'  -> '\n'
+        'r'  -> '\r'
+        't'  -> '\t'
 
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many $ noneOf "\"" <|> parseEscapeSequence
+  x <- many $ noneOf "\\\"" <|> parseEscapeSequence 
   char '"'
   return $ String x
 
@@ -43,15 +53,32 @@ parseAtom = do
              "#f" -> Bool False
              _    -> Atom atom
 
+baseToValidChars :: Int -> String
+baseToValidChars base = if base <= 10 then "" 
+                   else ['a' .. (toEnum $ ord 'a' + base - 11)]
+
+parseNumberInBase :: Integer -> Parser LispVal
+parseNumberInBase base = liftM (Number . toDecimal base) $ (many1 . oneOf) (map toUpper validAlphabets ++ validAlphabets ++ validDigits)
+  where validAlphabets = (baseToValidChars . fromIntegral) base
+        validDigits = ['0' .. toEnum $ fromEnum '0' + (min 9 (fromIntegral base - 1))]
+
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = (try $ string "0b" >> parseNumberInBase 2)
+           <|> (try $ string "0b" >> parseNumberInBase 8)
+           <|> (try $ string "0x" >> parseNumberInBase 16)
+           <|> parseNumberInBase 10
+
+toDecimal :: Integer -> String -> Integer
+toDecimal base s = foldl1 ((+) . (* base)) $ map (toInteger . digitToInt) s
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom 
+         <|> parseString 
+         <|> parseNumber
 
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
                    Left err -> "No Match: " ++ show err
-                   Right val -> "Found Value"
+                   Right val -> "Found Value: " ++ show val 
 
 
