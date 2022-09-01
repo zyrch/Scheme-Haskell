d47a8df1083c0e6a6f34907345d5dae85bdc026d
Rupanshu Yadav <58172798+zyrch@users.noreply.github.com>
2022-09-02 02:31:47 +0530

    Parser: added recursive parsing

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,7 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 import System.Environment
 import Data.Char(digitToInt, ord, intToDigit, toUpper)
-import Scheme.Parser.ParseAtom
-import Scheme.Parser.ParseString
-import Scheme.Parser.LispVal
-import Scheme.Parser.ParseNumber
+import Scheme.Parser.ParseExpr
 
 main :: IO ()
 main = do
@@ -15,15 +12,7 @@ main = do
   putStrLn $ "Parsing " ++ args !! 0
   putStrLn (readExpr $ args !! 0)
 
-parseExpr :: Parser LispVal
-parseExpr = parseCharacter
-         <|> parseString 
-         <|> parseNumber
-         <|> parseAtom
-
 readExpr :: String -> String
 readExpr input = case parse parseExpr "lisp" input of
                    Left err -> "No Match: " ++ show err
                    Right val -> "Found Value: " ++ show val 
-
-
---
+++ src/Scheme/Parser/ParseExpr.hs
@@ -0,0 +1,39 @@
+module Scheme.Parser.ParseExpr
+( parseExpr
+) where
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
+import Scheme.Parser.LispVal
+import Scheme.Parser.ParseString
+import Scheme.Parser.ParseNumber
+import Scheme.Parser.ParseAtom
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr = parseCharacter
+         <|> parseString 
+         <|> parseNumber
+         <|> parseAtom
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
